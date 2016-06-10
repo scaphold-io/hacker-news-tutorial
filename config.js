4aa94462ee4eadcf9aced777f3a84eced4b238b0
Vincent Ning <vincent.ning93@gmail.com>
2016-06-10 14:31:56 -0700

    clean app

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
+/**
+ * Modify the config Scaphold URL to point to your specific app.
+ * Find the URL at the top of the page on Scaphold.io once you've created an app.
+ * Yup. It's that easy.
+ */
+
 var config = {
 	scapholdUrl: "https://api.scaphold.io/graphql/9ef14f35-c459-41e9-a9d3-3933a7149822",
 }
--- js/components/App/Hero.js
+++ js/components/App/Hero.js
@@ -10,7 +10,7 @@ class Hero extends React.Component {
         <Col smOffset={2} sm={8}>
           <Jumbotron style={styles.jumbotron}>
             <h1>Welcome!</h1>
-            <p>Here you'll find Scaphold.io's Boilerplate React-Relay template :)</p>
+            <p>Here you'll find Scaphold.io's HackerNews Clone Tutorial :)</p>
             <p><Button bsStyle="primary" bsSize="large" target="_blank" href="https://scaphold.io">Learn more <FontAwesome name="check" /></Button></p>
             <p>Join our <a href="https://scapholdslackin.herokuapp.com/" target="_blank">Slack community</a>!</p>
           </Jumbotron>
