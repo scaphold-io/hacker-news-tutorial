8794531ff860abb0b84252951955c7efeb0bc62d
Vincent Ning <vincent.ning93@gmail.com>
2016-08-15 09:34:42 -0700

    null check for author email

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
  */
 
 var config = {
-	scapholdUrl: "https://api.scaphold.io/graphql/74ec6f85-faac-43b3-8ba6-d0ed29ef2f11",
+	scapholdUrl: "https://api.scaphold.io/graphql/74ec6f85-faac-43b3-8ba6-d0ed29ef2f11"
 }
 
 module.exports = config;
--- js/components/HackerNewsClone/HackerNewsItem.js
+++ js/components/HackerNewsClone/HackerNewsItem.js
@@ -6,12 +6,19 @@ class HackerNewsItem extends React.Component {
     let item = this.props.hnItem.node;
     let time = new Date(item.createdAt);
     time = time.toString();
-    let mailto = "mailto:" + item.author.username;
+    let mailto;
+    let email;
+    if (item.author && item.author.username) {
+      mailto = "mailto:" + item.author.username;
+      email = item.author.username;
+    }
+    mailto = "mailto:support@scaphold.io";
+    email = "support@scaphold.io";
 
     return (
       <div>
         <h1><em>{this.props.num + 1}.</em> <a href={item.url}>{item.title}</a></h1>
-        <h3>{item.score} points by <a href={mailto}>{item.author.username}</a></h3>
+        <h3>{item.score} points by <a href={mailto}>{email}</a></h3>
         <h5>at {time}</h5>
         <hr />
       </div>
